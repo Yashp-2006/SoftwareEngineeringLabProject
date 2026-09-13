122a475f5245a0584927b3eb36a48ff52366ae6d
Yash Pratap Singh <yash.singh.ug24@nsut.ac.in>
2026-09-13 19:50:09 +0530

    perf(client): remove deprecated eslint key from next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -8,10 +8,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   compress: true,
   experimental: {
     externalDir: true,
